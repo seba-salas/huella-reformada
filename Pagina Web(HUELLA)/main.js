015a0aca9091da62747dce6d5bff5c5d62d12e1d
seba-salas <68555779+seba-salas@users.noreply.github.com>
2020-07-20 11:48:26 -0400

    Add files via upload

---
+++ Pagina Web(HUELLA)/main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.hrr');
+ScrollReveal().reveal('.art-cards',{delay: 500});
+ScrollReveal().reveal('.cards-banner-one',{delay: 100});
+ScrollReveal().reveal('.social',{delay: 100});
